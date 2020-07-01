1cbdd66ec4158b3d40f7063ed4b807f52facc25a
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2020-07-01 17:59:11 +0200

    some changes more

--- iamaa/views.py
+++ iamaa/views.py
@@ -5,8 +5,9 @@ from .models import Welcome, ArtistIntroduction
 
 # Create your views here.
 def home(request):  ## rename to match imaa
-    artist = ArtistIntroduction.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #artist = ArtistIntroduction.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     welcome_text = Welcome.text
     return render(request, 'home.html', {'welcome_text' : welcome_text})
 
-
+def aboutus(request):
+    pass
