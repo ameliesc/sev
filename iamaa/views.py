0a9b8e431859236331eb6b94d7a54dce68824b3b
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2020-07-01 17:01:22 +0200

    added first views

--- iamaa/views.py
+++ iamaa/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import ArtistIntroduction
 # Create your views here.
-def posts(request):
-    return render(request, 'iamaa/posts.html', {})
+def posts(request):  ## rename to match imaa
+    artist = ArtistIntroduction.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'iamaa/posts.html', {'posts' : posts})
+
+
