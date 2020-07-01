e3288f5c1ec4c9c86f44c0f9c99748d2fb829ba1
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2020-07-01 19:21:25 +0200

    firt perfect version of copying home

--- iamaa/models.py
+++ iamaa/models.py
@@ -8,7 +8,7 @@ class ArtistIntroduction(models.Model):
     # interview_video = video is here either upload or reference from url 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    Text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     def publish(self):
--- iamaa/views.py
+++ iamaa/views.py
@@ -6,8 +6,8 @@ from .models import Welcome, ArtistIntroduction
 # Create your views here.
 def home(request):  ## rename to match imaa
     #artist = ArtistIntroduction.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    welcome_text = Welcome.text
-    return render(request, 'home.html', {'welcome_text' : welcome_text})
+    welcome_text = Welcome.objects.all()
+    return render(request, 'iamaa/home.html' , {'welcome_text' : welcome_text})
 
 def aboutus(request):
     pass
