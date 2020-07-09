293e987150f383d389147c5a667d762ff8208b83
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2020-07-09 22:42:08 +0200

    resetting

--- iamaa/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.static import static
-from django.conf import settings
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path(r'', views.home, name='home'),
-    path(r'',views.about, name='aboutpage')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
