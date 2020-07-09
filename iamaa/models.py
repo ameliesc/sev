41140df5d439ec1c521c0c7996026e95e7229027
Amelie Schmidt-Colberg <amelie.schmidtcolberg@gmail.com>
2020-07-09 18:46:24 +0200

    updated gitignore

--- iamaa/models.py
+++ iamaa/models.py
@@ -1,40 +1,18 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+import os
 # Create your models here.
 
-class ArtistIntroduction(models.Model):
-    # interview_video = video is here either upload or reference from url 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    Text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def add_video(self):
-        pass
-
-    def add_comment(self):
-        pass
-
-    def __str__(self):
-        return self.title
-
-
-class Comment(models.Model):
-    text = models.TextField()
-    created_date =  models.DateTimeField(default=timezone.now)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
 
 
 
 class Welcome(models.Model):
     text = models.TextField()
+
+class Description(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    icon_image = models.ImageField(db_column='imagefile', upload_to='media/', null=True, blank=True, help_text="Load an image.")
+    
